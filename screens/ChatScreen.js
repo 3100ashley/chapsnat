919d575d8d85a7eebc5eabc4e5543e4adb76de99
Ashley Manese <ashleymanese31@gmail.com>
2021-07-22 13:23:20 -0700

    updated Profile screen to be able to upload image from camera or library

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import db from "../firebase";
-import firebase from "firebase/app";
+import firebase from "@firebase/app";
 
 export default function ChatScreen({ route }) {
   const [messages, setMessages] = useState([]);
@@ -44,7 +44,7 @@ export default function ChatScreen({ route }) {
         // current "blue bubble" user
         _id: firebase.auth().currentUser.uid,
         name: firebase.auth().currentUser.displayName,
-        avatar: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+        avatar: firebase.auth().currentUser.photoURL,
       }}
       inverted={false}
       showUserAvatar={true}
